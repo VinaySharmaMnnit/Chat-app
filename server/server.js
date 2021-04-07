3b611b603b958dd3c91ebfc97fd49074041ad381
VinaySharmaMnnit <vinayshrotriya2021999@gmail.com>
2021-04-07 10:31:25 +0530

    Procees.env.PORT is added

--- server/server.js
+++ server/server.js
@@ -2,8 +2,14 @@
 
 const path= require('path');
 const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+var POST = process.env.PORT || '3000';
 
 var app  = express();
+//app contains this http.createServer(app) is doing all that is done by server in the background
+//ut to configure socket.io we used this server
+var server  = http.createServer(app);
 const PublicPath = path.join(__dirname,'../public/');
 
 app.use(express.static(PublicPath));
@@ -12,6 +18,6 @@ app.use(express.static(PublicPath));
 
 //console.log(PublicPath);
 
-app.listen('3000',()=>{
-    console.log("Starting server on port 3000");
+server.listen(PORT,()=>{
+    console.log(`Starting server on port ${PORT}`);
 })
